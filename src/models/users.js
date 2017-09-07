c78b00e09247312f8afac3a674f19d271a589c1f
hmygithub <2713237776@qq.com>
2017-09-07 16:30:59 +0800

    Step 6 添加界面，让用户列表展现出来

--- src/models/users.js
+++ src/models/users.js
@@ -4,18 +4,26 @@ export default {
   namespace: 'users',
   state: {
     list: [],
-    total: null
+    total: null,
+    page: null
   },
   reducers: {
-    save(state, { payload: { data: list, total } }) {
-      return {...state, list, total };
+    save(state, { payload: { data: list, total, page } }) {
+      return {...state, list, total, page };
     }
   },
   effects: {
-    *fetch({ payload: { page } }, { call, put }) {
-     const { data, headers } = yield call(usersService.fetch, { page });
-     yield put({ type: 'save', payload: { data, total: headers['x-total-count'] } });
-     }
+    *fetch({ payload: { page = 1 } }, { call, put }) {
+      const { data, headers } = yield call(usersService.fetch, { page });
+      yield put({
+       type: 'save',
+       payload: {
+         data,
+         total: parseInt(headers['x-total-count'], 10),
+         page: parseInt(page, 10)
+       }
+      });
+    }
   },
   subscriptions: {
     setup({ dispatch, history }) {
--- src/services/users.js
+++ src/services/users.js
@@ -2,7 +2,8 @@
  * Created by lenovo on 2017/9/7.
  */
 import request from '../utils/request.js';
+import { PAGE_SIZE } from '../constants';
 
-export function fetch({ page = 1 }) {
-  return request(`/api/users?_page=${page}&_limit=5`);
+export function fetch({ page }) {
+  return request(`/api/users?_page=${page}&_limit=${PAGE_SIZE}`);
 }
